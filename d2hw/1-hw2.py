ceabebf78687d3f76c13f3063da4a8ee56e6ed0d
Dan Konzman <dkonzman@elon.edu>
2016-08-30 19:10:55 -0400

    file and output for part 1 now stripped of spaces

--- d2hw/1-hw2.py
+++ d2hw/1-hw2.py
@@ -5,9 +5,18 @@ import sys
 for line in sys.stdin:
     if "DROME" in line:
         strline = line.rstrip("\n")
-        a = strline[(len(strline)-23):]
-        print a[0:10], "\t", a[12:]
+        first_space = strline[(len(strline)-23):(len(strline)-13)]
+        uniprot = strline[(len(strline) - 11):]
+        flybase = first_space.strip(" ")
+        print flybase, "\t", uniprot
         
+        
+        #flybase = bothcol[0:10]
+        #uniprot = bothcol[12:]
+        #print flybase, "\t", uniprot
+        
+        
+        #print "".join(flybase), "\t", uniprot
         
         #for char in strline:
         #    if char = " "
